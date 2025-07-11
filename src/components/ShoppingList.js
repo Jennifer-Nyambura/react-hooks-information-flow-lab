b0176c79e5da6c56745299ef34a2a201298613dc
Jennifer <jenninyambu@gmail.com>
2025-07-12 00:44:23 +0300

    all changes

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
+import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+function ShoppingList({
+  items,
+  selectedCategory,
+  onCategoryChange,
+}) {
+  const [internalCategory, setInternalCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
+  const category = selectedCategory ?? internalCategory; // Use prop or internal
+  const handleChange = onCategoryChange ?? setInternalCategory; // Use prop or internal
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    if (category === "All") return true;
+    return item.category === category;
   });
 
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+      <Filter
+        selectedCategory={category}
+        onCategoryChange={handleChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
